fa257acffe43c86cd84d3ea8e4d5812b4062b464
billyzou <yz3352@nyu.edu>
2018-11-26 15:27:37 -0500

    feat: add titles to home page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+    faGraduationCap,
+    faCode,
+    faGamepad,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+library.add(faGamepad);
+library.add(faCode);
+library.add(faGraduationCap);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,24 @@
 <template>
     <div>
         <navigation></navigation>
+        <main class="main-container">
+            <header class="intro">
+                <h1 class="intro__name">Yijie "Billy" Zou</h1>
+                <p class="intro__title">
+                    <font-awesome-icon icon="graduation-cap" />
+                    Student major in Computer Science & Interactive Media Arts at NYU
+                </p>
+                <p class="intro__title">
+                    <font-awesome-icon icon="code" />
+                    Web front-end developer
+                </p>
+                <p class="intro__title">
+                    <font-awesome-icon icon="gamepad" />
+                    Game lover and amateur game developer
+                </p>
+            </header>
+            <img class="photo" src="../assets/img/photo.jpeg" />
+        </main>
     </div>
 </template>
 
@@ -16,7 +34,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import "../assets/scss/main";
+
     .nav {
         display: flex;
+    }
+
+    .main-container {
+        display: flex;
+        justify-content: center;
+        width: 80%;
+        margin: 50px 10%;
+    }
+
+    .photo {
+        width: 360px;
+        height: 480px;
+        border-radius: 10px;
+        margin-left: 5%;
+    }
+
+    .intro {
+        width: 360px;
+    }
+
+    .intro__name {
+        color: $blue;
+        font-size: 3rem;
+    }
+
+    .intro__title {
+        color: $light-black;
+        font-size: 1.5rem;
+        line-height: 2rem;
+        &:before {
+            content: '';
+        }
     }
 </style>
