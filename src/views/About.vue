1cd0a8dfa1a1f0a6b829f3204231588a8e8d1b7a
billyzou <yz3352@nyu.edu>
2018-12-04 00:28:21 -0500

    feat: add animation in the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,36 +2,48 @@
     <div>
         <navigation></navigation>
         <main class="container container--narrow">
-            <h2 class="title">About Billy</h2>
-            <article>
-                <p class="paragraph">
-                    Billy is currently a junior student at
-                    NYU Shanghai. He double majors in Computer Science & Interactive Media Arts.
-                    He is a junior web front-end developer, who started doing front-end development
-                    work in 2017.
-                </p>
-                <p class="paragraph">
-                    He started coding in freshman year with Processing as the first programming
-                    language he learned. He declared major as Computer Science the second semester
-                    and started learning programming systematically using Python. Apart from the
-                    fundamental computer science courses taken in school, Billy spent much time
-                    studying web development in his spare time. His web development skills are
-                    mainly acquired through self-studying.
-                </p>
-                <p class="paragraph">
-                    JavaScript is Billy's primary programming language. He is familiar with
-                    Javascript, including ES6 and newer syntax. He is also familiar with Sass and
-                    some new CSS features such as flexbox and grid. He got started front-end
-                    development using jQuery and Bootstrap. His first professional front-end work
-                    was at <a class="link" href="https://en.wikipedia.org/wiki/Vipshop">Vip.com</a>
-                    where he became experienced in
-                    <a class="link"
-                       href="https://walkthechat.com/wechat-mini-programs-simple-introduction/">
-                        Wechat Mini Program
-                    </a> and Vue.js as an intern front-end engineer. Now Billy mainly works with
-                    Vue.js, Node.js and MongoDB. With his Python experience, he is also able to
-                    develop web application using frameworks like Flask and Django.
-                </p>
+            <span class="title" :class="{'title--deactive': !aboutIsActive}" @click="toggleActive">
+                About Billy
+            </span>
+            <span class="title" :class="{'title--deactive': aboutIsActive}" @click="toggleActive">
+                Tech Stack
+            </span>
+            <article class="intro">
+                <template v-if="aboutIsActive">
+                    <p class="intro__para">
+                        Billy is currently a junior student at
+                        NYU Shanghai. He double majors in Computer Science & Interactive Media Arts.
+                        He is a junior web front-end developer, who started doing front-end
+                        development
+                        work in 2017.
+                    </p>
+                    <p class="intro__para">
+                        He started coding in freshman year with Processing as the first programming
+                        language he learned. He declared major as Computer Science the second
+                        semester
+                        and started learning programming systematically using Python. Apart from the
+                        fundamental computer science courses taken in school, Billy spent much time
+                        studying web development in his spare time. His web development skills are
+                        mainly acquired through self-studying.
+                    </p>
+                    <p class="intro__para">
+                        JavaScript is Billy's primary programming language. He is familiar with
+                        Javascript, including ES6 and newer syntax. He is also familiar with Sass
+                        and
+                        some new CSS features such as flexbox and grid. He got started with
+                        front-end
+                        development using jQuery and Bootstrap. His first professional front-end
+                        work
+                        was at <a class="intro__link" href="https://en.wikipedia.org/wiki/Vipshop">Vip.com</a>
+                        where he became experienced in
+                        <a class="intro__link"
+                           href="https://walkthechat.com/wechat-mini-programs-simple-introduction/">
+                            Wechat Mini Program
+                        </a> and Vue.js as an intern front-end engineer. Now Billy mainly works with
+                        Vue.js, Node.js and MongoDB. With his Python experience, he is also able to
+                        develop web application using frameworks like Flask and Django.
+                    </p>
+                </template>
             </article>
         </main>
     </div>
@@ -43,6 +55,16 @@ import Navigation from '../components/Navigation.vue';
 export default {
     name: 'About',
     components: { Navigation },
+    data() {
+        return {
+            aboutIsActive: true,
+        };
+    },
+    methods: {
+        toggleActive() {
+            this.aboutIsActive = !this.aboutIsActive;
+        },
+    },
 };
 </script>
 
@@ -50,20 +72,36 @@ export default {
     @import "../assets/scss/main";
 
     .title {
+        position: absolute;
         font-size: 2.5em;
         color: $blue;
+        transition: 0.5s;
+        cursor: pointer;
+        z-index: 100;
+        left: 20%;
     }
 
-    .paragraph {
+    .title--deactive {
+        left: calc(20% + 6em);
+        color: $color-disable;
+    }
+
+    .intro {
+        position: relative;
+        padding-top: 2.5em;
+    }
+
+    .intro__para {
         color: $light-black;
         font-size: 1.5em;
         line-height: 1.5em;
+        @include fade-in;
         &:before {
             content: '\00a0\00a0';
         }
     }
 
-    .link {
+    .intro__link {
         color: $light-black;
         &:hover {
             color: $blue;
