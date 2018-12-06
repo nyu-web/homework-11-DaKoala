81780163ef5260cc0efd38d7ec02c3fc2a5f62ab
billyzou <yz3352@nyu.edu>
2018-12-06 18:21:07 -0500

    feat: add Google Analytics

--- src/router.js
+++ src/router.js
@@ -1,10 +1,16 @@
+/* eslint-disable */
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const ENABLE_GA = true;
+const isLocalhost = window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/);
+
+const router = new Router({
     routes: [
         {
             path: '/',
@@ -23,3 +29,26 @@ export default new Router({
         },
     ],
 });
+
+if (ENABLE_GA) {
+    router.onReady(() => {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-130553498-1', 'auto');
+        ga('send', 'pageview');
+
+        if (isLocalhost) {
+            ga('set', 'sendHitTask', null);
+        }
+
+        router.afterEach((to) => {
+            ga('set', 'page', to.fullPath);
+            ga('send', 'pageview');
+        })
+    });
+}
+
+export default router;
